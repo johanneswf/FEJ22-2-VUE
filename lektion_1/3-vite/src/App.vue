6f404d2ea276d177e183c8161732b1f5c63c4648
Joakim <wahlstrom.joakim@gmail.com>
2022-11-22 10:48:14 +0100

    lektion 1

---
+++ lektion_1/3-vite/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>{{ title }}</p>
+  <p>{{ count }}</p>
+  <button v-on:click="count++">öka</button>
+  <button @click="count--">minska</button>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: 'Hej från Vue',
+        count: 0
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
